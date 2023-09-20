0c2da133de8054875a10cdc3c1a5f10f3c1a1119
xansiety <ferando543@outlook.com>
2023-09-19 19:40:51 -0600

    filters and sorting on app added

--- ExampleGraphQL/GraphQL/Query.cs
+++ ExampleGraphQL/GraphQL/Query.cs
@@ -9,6 +9,8 @@ namespace ExampleGraphQL.GraphQL
         [UseDbContext(typeof(ApplicationDbContext))] // to enable multiple instances of the ApplicationDbContext for every complex query not necessary for simple queries
         //public IQueryable<Video> GetVideos([Service] ApplicationDbContext context) // for common queries and one instance of the ApplicationDbContext
         [UseProjection] // enable projections this enables the client to specify the fields that they want to receive from the server for example equivalent to include() in EF Core
+        [UseFiltering] // enable filtering
+        [UseSorting] // enable sorting
         public IQueryable<Video> GetVideos([ScopedService] ApplicationDbContext context)
         {
             return context.Videos;
@@ -16,6 +18,8 @@ namespace ExampleGraphQL.GraphQL
 
         [UseDbContext(typeof(ApplicationDbContext))]
         [UseProjection]
+        [UseFiltering]
+        [UseSorting]
         public IQueryable<Director> GetDirectors([ScopedService] ApplicationDbContext context)
         {
             return context.Directores;

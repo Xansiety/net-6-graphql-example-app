a49b89c97e8029136d7272f3cb58698ab4ee1523
xansiety <ferando543@outlook.com>
2023-09-19 20:11:14 -0600

    paging results and sorter

--- ExampleGraphQL/GraphQL/Query.cs
+++ ExampleGraphQL/GraphQL/Query.cs
@@ -8,9 +8,10 @@ namespace ExampleGraphQL.GraphQL
         // using HotChocolate package to create a GraphQL API
         [UseDbContext(typeof(ApplicationDbContext))] // to enable multiple instances of the ApplicationDbContext for every complex query not necessary for simple queries
         //public IQueryable<Video> GetVideos([Service] ApplicationDbContext context) // for common queries and one instance of the ApplicationDbContext
+        [UsePaging] // enable paging -> ITS VERY IMPORTAN THE ORDER OF THE ATTRIBUTES  FOR ENABLE PAGING, FILTERING AND SORTING | PAGING MUST BE FIRST AND ABOVE THE OTHERS AND ALWAS UNDER THE USEDBCONTEXT ATTRIBUTE
         [UseProjection] // enable projections this enables the client to specify the fields that they want to receive from the server for example equivalent to include() in EF Core
         [UseFiltering] // enable filtering
-        [UseSorting] // enable sorting
+        [UseSorting] // enable sorting        
         public IQueryable<Video> GetVideos([ScopedService] ApplicationDbContext context)
         {
             return context.Videos;
